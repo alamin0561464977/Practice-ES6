41a484b34cd1a1158bf0b2ec41aa6fe0a0ab32f2
alamin0561464977 <amin23435678901315@gmail.com>
2022-08-24 10:43:41 +0300

    added a sentence

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 1) Write an arrow function that will take 3 parameters, will multiply
 the parameters and will return the result.
 */
-const arrowFunctionMultiply = (x, y, z) => x + y + z;
-const total1 = arrowFunctionMultiply(20, 10, 30);
+const arrowFunctionMultiplyParameters = (x, y, z) => x + y + z;
+const total1 = arrowFunctionMultiplyParameters(20, 10, 30);
 // console.log(total1);
 
 // ============================================================
 /*
 2) Write the following sentence in three lines and print the result:
 I am a web developer. I love to code. I love to eat biryani.
-*/ 
+*/
+const sentence = `
+    I am web developer
+    I love to code 
+    I love to eat biryani
+`;
+console.log(sentence);
