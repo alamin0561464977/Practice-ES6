c1eecc3ef9b0ae49bbcf01d6c02d5e07652d20d4
alamin0561464977 <amin23435678901315@gmail.com>
2022-08-24 10:38:37 +0300

    added a arrow function maltiply parameters

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+1) Write an arrow function that will take 3 parameters, will multiply
+the parameters and will return the result.
+*/
+const arrowFunctionMultiply = (x, y, z) => x + y + z;
+const total1 = arrowFunctionMultiply(20, 10, 30);
+// console.log(total1);
+
+// ============================================================
+/*
+2) Write the following sentence in three lines and print the result:
+I am a web developer. I love to code. I love to eat biryani.
+*/ 
